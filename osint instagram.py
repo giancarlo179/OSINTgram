b2c0c62b1f9b75e70b294e8d731d9a5a4bace801
giancarlo179 <32601403+giancarlo179@users.noreply.github.com>
2023-01-26 22:23:02 -0400

    Add files via upload

---
+++ osint instagram.py
@@ -0,0 +1,24 @@
+import instaloader
+from getpass import getpass
+
+# Instantiate the API client
+L = instaloader.Instaloader()
+
+# Set your username and password
+L.context.log("Setting username and password")
+L.context.username = getpass("Insert your Instagram account: ")
+L.context.password = getpass("Insert your password: ")
+
+# Target user's profile
+target_username = input("Insert target user profile: ")
+
+# Get the user's profile
+profile = instaloader.Profile.from_username(L.context, target_username)
+
+# Print the user's information
+print("Username:", profile.username)
+print("Full name:", profile.full_name)
+print("Biography:", profile.biography)
+print("Followers:", profile.followers)
+print("Following:", profile.followees)
+print("Number of posts:", profile.mediacount)
